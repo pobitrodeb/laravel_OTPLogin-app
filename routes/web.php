6dd68d3c70c7aeece553b665a2d46ebbba0b530a
Pobitrodeb <pobitrodn@gmail.com>
2023-02-01 21:50:52 +0600

    web file e method added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +20,12 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::controller(App\Http\Controllers\Auth\AuthOtpController::class)->group(function(){
+    Route::get('otp/login', 'login')->name('otp.login');
+    Route::post('otp/generate', 'generate')->name('otp.generate');
+    Route::get('otp/verification/{user_id}', 'verification')->name('otp.verification');
+    Route::post('otp/login', 'loginWithOtp')->name('otp.getlogin');
+});
+
+
